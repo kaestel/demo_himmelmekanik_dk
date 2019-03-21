64591d80860403c680fdf04f858af1d6383808fa
threepwoodd <scope.dk@gmail.com>
2019-03-21 22:59:05 +0100

    Further fixes and cuts

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -3,7 +3,6 @@ document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2-himme
 
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-page.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-login.js"></script>');
 
 
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-letter.js"></script>');
--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -3,7 +3,6 @@ document.write('<script type="text/javascript" src="/js/manipulator/v0_9_2-himme
 
 document.write('<script type="text/javascript" src="/js/lib/smartphone/i-page.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/smartphone/i-front.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-login.js"></script>');
 
 
 document.write('<script type="text/javascript" src="/js/lib/smartphone/i-letter.js"></script>');
